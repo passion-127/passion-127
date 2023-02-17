6ffbda8106adee435c86d262d7fb07e8aead4a08
passion-127 <123952026+passion-127@users.noreply.github.com>
2023-02-17 10:18:02 +0900

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,13 @@
+var script = document.createElement('script');
+script.onload = function () {
+    //do stuff with the script
+    $(document).ready(function() {
+       alert("Hello World!");
+      if($('form[data-type=add-to-cart-form] > div.product-form__buttons').length) {
+        $('form[data-type=add-to-cart-form] > div.product-form__buttons').append("<Button class='product-form'>Try Now</Button>");
+      }
+    });
+};
+script.src = "https://code.jquery.com/jquery-3.6.3.slim.min.js";
+
+document.head.appendChild(script);
